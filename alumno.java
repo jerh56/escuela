48c0ee16c9ed1d9f5893fe82a064827d56ec82f4
Juan Ramos <jerh56@gmail.com>
2018-02-03 10:51:15 -0600

    Intercambio inicial de proyecto

---
+++ alumno.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class alumno here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class alumno extends personal
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class alumno
+     */
+    public alumno()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    public alumno(int edad)
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
